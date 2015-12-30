80f62a2acaeab59042eb70956d1e1ae6535c45c5
bala-vspg <bala.vspg@gmail.com>
2015-12-29 16:05:58 -0800

    Add String Compression Problem

---
+++ StringCompression/StringCompression/Compresser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace StringCompression
+{
+   public class Compress
+    {
+        private void isValid(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                throw new ArgumentException();
+        }
+
+        public string CompressGivenString(string input)
+        {
+            isValid(input);
+
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            
+            for(int candidate=0,current=candidate+1;candidate<input.Length;)
+            {
+                if((current<input.Length) && (input[candidate] ==input[current]))
+                {
+                    count++;
+                    current++;
+        
+                }
+                else
+                {
+                    sb.Append(input[candidate]);
+                    sb.Append(count);
+                    count = 0;
+                    candidate = current;
+                }
+                
+            }
+            if (input.Length >= sb.ToString().Length)
+                return sb.ToString();
+            else
+                return input;
+        }
+    }
+}
---
+++ StringCompression/StringCompressionTests/CompressTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCompression;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+
+namespace StringCompression.Tests
+{
+    [TestClass()]
+    public class CompressTests
+    {
+        [TestMethod()]
+        public void CompressGivenStringTest()
+        {
+            string s = "aaabbbccddeeeffff";
+            Compress c = new Compress();
+            c.CompressGivenString(s).Should().Be("a3b3c2d2e3f4");
+        }
+
+        [TestMethod()]
+        public void CompressGivenStringTestWithUniqueChars()
+        {
+            string s = "abcdefgh";
+            Compress c = new Compress();
+            c.CompressGivenString(s).Should().Be("abcdefgh");
+        }
+
+        [TestMethod()]
+        public void CompressGivenStringTestWithNullInput()
+        {
+            string s = null;
+            Compress c = new Compress();
+            Action action=()=> c.CompressGivenString(s);
+            action.ShouldThrow<ArgumentException>();
+        }
+
+        [TestMethod()]
+        public void CompressGivenStringTestWithEmptyInput()
+        {
+            string s = null;
+            Compress c = new Compress();
+            Action action = () => c.CompressGivenString(s);
+            action.ShouldThrow<ArgumentException>();
+        }
+
+        [TestMethod()]
+        public void CompressGivenStringTestWithLowerAndUpperCaseLetters()
+        {
+            string s = "aaabbFFFAAABBB";
+            Compress c = new Compress();
+            c.CompressGivenString(s).Should().Be("a3b2F3A3B3");
+        }
+
+    }
+}
