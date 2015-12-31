df41cba47ee272fb80b78782ba40e691b335211e
bala-vspg <bala.vspg@gmail.com>
2015-12-31 12:27:46 -0800

    Add Majority CHecker

---
+++ MajorityElementChecker/MajorityElementChecker/MajorityElement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace MajorityElementChecker
+{//find teh majority element in the array. MajorityELment=occurs morethan n/2 times.
+    //Question - what if we have more than one majority element? can i return an array or stack which has the collection of such elements - ?
+    public class MajorityElement
+    {
+        public int FindMajorityElement(int[] input)
+        {
+            Dictionary<int,int> occuranceCounter=new Dictionary<int, int>();
+            foreach (var i in input)
+            {
+                if (occuranceCounter.ContainsKey(i))
+                {
+                    occuranceCounter[i]++;
+                    if (occuranceCounter[i] > (input.Length/2))
+                        return i;
+                }
+                else
+                {
+                    occuranceCounter.Add(i,1);
+                }
+                   
+            }
+            return 0;
+        }
+    }
+}
---
+++ MajorityElementChecker/MajorityElementCheckerTests/MajorityElementTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MajorityElementChecker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace MajorityElementChecker.Tests
+{
+    [TestClass()]
+    public class MajorityElementTests
+    {
+        [TestMethod()]
+        public void FindMajorityElementTest()
+        {
+            MajorityElement ME=new MajorityElement();
+            ME.FindMajorityElement(new int[] {4, 4, 3, 5, 4, 4, 6, 7,4,4}).Should().Be(4);
+        }
+    }
+}
